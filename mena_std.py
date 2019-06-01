f97fa70ed137dec40ec0ac87866b9bad8b608c30
Armin Mehri <51209972+Rm1n90@users.noreply.github.com>
2019-06-01 17:24:05 +0200

    Update V1.1

--- mena_std.py
+++ mena_std.py
@@ -1,38 +1,41 @@
+
+
+"""
+computing the mean and the standard deviation per channel of any datasets with PyTorch
+"""
 import numpy as np
-import os
-import glob
-import cv2
-from PIL import Image
 import torch
 import torch.nn as nn
 import torchvision
-import glob
-
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 from torchvision import transforms
 
-def mean__std(data_loader):
-    cnt = 0
+Path_to_Dataset = " "
+def mean_std(data):
+    count = 0
     mean = torch.empty(3)
     std = torch.empty(3)
     # import pdb;
     # pdb.set_trace()
-
-    for data, label in data_loader:
+    for data, label in data:
         b, c, h, w = data.size()
-        nb_pixels = b * h * w
-        sum_ = torch.sum(data, dim=[0, 2, 3])
-        sum_of_square = torch.sum(data ** 2, dim=[0, 2, 3])
-        mean = (cnt * mean + sum_) / (cnt + nb_pixels)
-        std = (cnt * std + sum_of_square) / (cnt + nb_pixels)
+        num_pixels = b * h * w
+        _sum = torch.sum(data, dim=[0, 2, 3])
+        square = torch.sum(data ** 2, dim=[0, 2, 3])
+        mean = (count * mean + _sum) / (cnt + num_pixels)
+        std = (count * std + square) / (cnt + num_pixels)
+        std = torch.sqrt(std - mean ** 2)
 
-        cnt += nb_pixels
+        count += num_pixels
+        
+    return mean, std 
 
-    return mean, torch.sqrt(std - mean ** 2)
+data_loader = torch.utils.data.DataLoader(torchvision.datasets.ImageFolder("Path_to_Dataset",
+                                                                           transform=transforms.Compose
+                                                                           ([transforms.ToTensor()])),
+                                                                            batch_size=1, shuffle=False, num_workers=4)
 
-train_data = torchvision.datasets.ImageFolder('b', transform=transforms.Compose([transforms.ToTensor()]))
-data_loader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=False, num_workers=4)
-
-mean, std = mean__std(data_loader)
+mean, std = mean_std(data_loader)
 print(mean, std)
+# output --> tensor([X.XXX, X.XXX, X.XXX], [X.XXX, X.XXX, X.XXX])
